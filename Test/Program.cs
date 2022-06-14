b5d5334171add51f0c56b6e577e1c87f9ce6705e
Ivan <vana.antropov@gmail.com>
2022-06-14 21:18:24 +0500

    Version 0.04 Try use null for help and result in solveEx, its not work right and i dont know how fix

--- Test/Program.cs
+++ Test/Program.cs
@@ -16,7 +16,7 @@ string[] CreatArray()
 string[] SolveEx(string[] array)
 {
 
-    string help = string.Empty;
+    string help = null;
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i].Length <= 3)
@@ -31,30 +31,21 @@ string[] SolveEx(string[] array)
 
 string Print(string[] array)
 {
-    string forPrint = string.Empty;
-    if (array.Length == 0)
+   string forPrint = string.Empty;
+    forPrint += "[";
+    if (array == null)
     {
-        return forPrint += " Array is empty ";
+        forPrint += " Array is empty ";
+        
     }
     else
     {
-        forPrint += "[";
         for (int i = 0; i < array.Length; i++)
         {
-            if (i == array.Length - 1)
-            {
-                forPrint += array[i];
-            }
-            else
-            {
-                forPrint += array[i] + ", ";
-            }
-
+            forPrint += array[i] + ", ";
         }
-        forPrint += "]";
-        return forPrint;
-
+        forPrint = forPrint.TrimEnd(',',' ');
     }
-
-
+    forPrint += "]";
+    return forPrint;
 }
