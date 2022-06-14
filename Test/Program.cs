e6504980be224ddb89a12d8f213a4fdfb83faebe
Ivan <vana.antropov@gmail.com>
2022-06-14 20:13:16 +0500

    Version 0.01 did rough solution with const array

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,19 +1,44 @@
 ﻿
-CreatArray();
-SolveEx();
-Print();
+string[] arr = { "as", "as", "13f", "adkook", "4okko", "fhg" };
 
+//CreatArray();
+
+
+Console.WriteLine(Print(SolveEx(arr)));
+
+/*
 string[] CreatArray()
 {
 
 }
+*/
 
 string[] SolveEx(string[] array)
 {
+
+    string help = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            help += array[i] + " ";
+        }
+    }
+    string[] result = help.Split(" ");
+    return result;
 
 }
 
 string Print(string[] array)
 {
+    string forPrint = string.Empty;
+    forPrint += "[";
+    
+        for (int i = 0; i < array.Length; i++)
+        {
+            forPrint += array[i] + ", ";
+        }
+    forPrint += "]";
+    return forPrint;
 
 }
